8ac3c98d9fcf737f719947cb7055fe45551f0c18
unknown <afbetancurtb@uqvirtual.edu.co>
2022-10-05 21:24:40 -0500

    prueba6

--- src/main/java/com/example/demo44/entities/Category.java
+++ src/main/java/com/example/demo44/entities/Category.java
@@ -17,7 +17,7 @@ public class Category implements Serializable {
 
     private String description;
 
-    @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "category")
+    @OneToMany(cascade = {CascadeType.ALL}, mappedBy = "category")
     @JsonIgnoreProperties("category")
     private List<Machine> machines;
 
