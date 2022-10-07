8f7757ab1991ce37875d9bd33f63a1a9dd6b9280
unknown <afbetancurtb@uqvirtual.edu.co>
2022-10-06 21:09:43 -0500

    prueba11

--- src/main/java/com/example/demo44/controller/pruebasController.java
+++ src/main/java/com/example/demo44/controller/pruebasController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/Reservation") 
+@RequestMapping("/api/pruebas")
 public class pruebasController {
 
     @Autowired
