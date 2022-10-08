ad78fd16d5b1f7bac72d284b0cfdb5916abb351a
unknown <afbetancurtb@uqvirtual.edu.co>
2022-10-08 14:57:40 -0500

    prueban

--- src/main/java/com/example/demo44/entities/Machine.java
+++ src/main/java/com/example/demo44/entities/Machine.java
@@ -25,11 +25,11 @@ public class Machine implements Serializable {
     private Category category;
 
     @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "machine")
-    @JsonIgnoreProperties({"machines","client"})
+    @JsonIgnoreProperties({"machine","client"})
     private List<Message> messages;
 
-    @OneToMany(cascade = {CascadeType.ALL}, mappedBy = "machines")
-    @JsonIgnoreProperties({"machines","messages","reservations"})
+    @OneToMany(cascade = {CascadeType.ALL}, mappedBy = "machine")
+    @JsonIgnoreProperties({"machine","messages"})
     private List<Reservation> reservations;
 
     public Integer getID(){
